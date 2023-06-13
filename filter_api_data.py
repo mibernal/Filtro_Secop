e0a1ad453b54c406b9de658e1cd499b62b0fb3a0
Miguel Bernal <65865138+mibernal@users.noreply.github.com>
2023-06-13 16:09:37 -0500

    Correccion Python

--- filter_api_data.py
+++ filter_api_data.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from sodapy import Socrata
 
+def render_table(data):
+    table = pd.DataFrame.from_records(data)
+    print(table)
+
 # Autenticación del cliente con el token de la aplicación y las credenciales de usuario
 client = Socrata("www.datos.gov.co", "qrWbZ9mhsAXj6S0L2U3LMV2YF", username="miguelangelbernal1991@gmail.com", password="Mid@910625")
 
 # Obtener los resultados de la consulta
-results = client.get("f789-7hwg", nombre_entidad="CUNDINAMARCA ALCALDIA MUNICIPIO DE CACHIPAY", limit=2000)
+results = client.get("f789-7hwg", where="fecha_de_firma_del_contrato BETWEEN '2019-01-01' AND '" + pd.Timestamp.now().strftime('%Y-%m-%d') + "'", municipio_entidad="Cachipay", limit=5000)
 
-# Convertir los resultados en un DataFrame de pandas
-results_df = pd.DataFrame.from_records(results)
-
-# Imprimir el DataFrame
-print(results_df)
+# Renderizar la tabla
+render_table(results)
