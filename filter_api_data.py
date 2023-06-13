4d59eaa37b5d23e6407e1027956a626a00e6f9bc
Miguel Bernal <65865138+mibernal@users.noreply.github.com>
2023-06-13 17:13:26 -0500

    output csv

--- filter_api_data.py
+++ filter_api_data.py
@@ -1,9 +1,17 @@
 import pandas as pd
 from sodapy import Socrata
 
-def render_table(data):
+def render_table(data, output_format):
     table = pd.DataFrame.from_records(data)
-    print(table)
+    
+    if output_format == 'csv':
+        table.to_csv('output.csv', index=False)
+        print("Datos guardados en output.csv")
+    elif output_format == 'excel':
+        table.to_excel('output.xlsx', index=False)
+        print("Datos guardados en output.xlsx")
+    else:
+        print("Formato de salida no válido. Use 'csv' o 'excel'.")
 
 # Autenticación del cliente con el token de la aplicación y las credenciales de usuario
 client = Socrata("www.datos.gov.co", "qrWbZ9mhsAXj6S0L2U3LMV2YF", username="miguelangelbernal1991@gmail.com", password="Mid@910625")
@@ -11,5 +19,5 @@ client = Socrata("www.datos.gov.co", "qrWbZ9mhsAXj6S0L2U3LMV2YF", username="migu
 # Obtener los resultados de la consulta
 results = client.get("f789-7hwg", where="fecha_de_firma_del_contrato BETWEEN '2019-01-01' AND '" + pd.Timestamp.now().strftime('%Y-%m-%d') + "'", municipio_entidad="Cachipay", limit=5000)
 
-# Renderizar la tabla
-render_table(results)
+# Renderizar y guardar la tabla en el formato deseado
+render_table(results, output_format='csv')
